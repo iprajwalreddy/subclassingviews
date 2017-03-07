e6675e39934364afa2180daca541c7d1725e25a1
P Reddy <prajwalreddy@Ps-iMac.local>
2017-03-07 22:54:20 +0530

    Initial Commit

---
+++ custom-view/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  custom-view
+//
+//  Created by Prajwal Reddy on 07/03/17.
+//  Copyright © 2017 Prajwal Reddy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
